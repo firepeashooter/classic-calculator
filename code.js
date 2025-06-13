176da6dba1c570d37f34d98fffc5add769b379f6
Ben Braun <benjamin.braun2003@gmail.com>
2025-06-13 18:12:52 -0400

    Create functions for calculator operators

---
+++ code.js
@@ -0,0 +1,33 @@
+function add(num1, num2){
+    
+    return num1 + num2;
+}
+
+
+function subtract(num1, num2){
+
+    return num1 - num2
+}
+
+
+function multiply(num1, num2){
+
+    return num1 * num2;
+}
+
+
+function divide(num1, num2){
+
+    return (num1 / num2).toFixed(2);
+}
+
+
+
+num1 = 3
+num2 = 5
+
+
+console.log(add(num1, num2));
+console.log(subtract(num1, num2));
+console.log(multiply(num1, num2));
+console.log(divide(num1, num2));
