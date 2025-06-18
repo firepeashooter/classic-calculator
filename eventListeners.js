acd0ab34a25d02c2bb9d54d8c89de5af129169e1
Ben Braun <benjamin.braun2003@gmail.com>
2025-06-18 16:13:47 -0400

    Clear screen when a new number is inputted after answer

--- eventListeners.js
+++ eventListeners.js
@@ -2,6 +2,8 @@ let buttons = document.querySelector(".input");
 let display = document.querySelector(".content");
 let clear = document.querySelector("#clear");
 
+let answerDisplayed = false;
+
 
 
 clear.addEventListener("click", () => {
@@ -22,83 +24,41 @@ buttons.addEventListener("click", (e) => {
     switch(target.id){
 
         case "one":
-            if (num1){
-                display.textContent = "1";
-            }else{
-                display.textContent += "1";
-            }
-            break;
 
         case "two":
-            if (num1){
-                display.textContent = "2";
-            }else{
-                display.textContent += "2";
-            }
-            break;
+            
         case "three":
-            if (num1){
-                display.textContent = "3";
-            }else{
-                display.textContent += "3";
+
+        //Clears the Display if answer is displayed so we can write new numbers without adding to the subtotal
+            if (answerDisplayed){
+                display.textContent = "";
+                answerDisplayed = false;
             }
+            
+            display.textContent += "3";
+            
             break;
 
         case "four":
-            if (num1){
-                display.textContent = "4";
-            }else{
-                display.textContent += "4";
-            }
-            break;
+           
 
         case "five":
-            if (num1){
-                display.textContent = "5";
-            }else{
-                display.textContent += "5";
-            }
-            break;
+            
 
         case "six":
-            if (num1){
-                display.textContent = "6";
-            }else{
-                display.textContent += "6";
-            }
-            break;
+            
 
         case "seven":
-            if (num1){
-                display.textContent = "7";
-            }else{
-                display.textContent += "7";
-            }
-            break;
+            
 
         case "eight":
-            if (num1){
-                display.textContent = "8";
-            }else{
-                display.textContent += "8";
-            }
-            break;
+            
 
         case "nine":
-            if (num1){
-                display.textContent = "9";
-            }else{
-                display.textContent += "9";
-            }
-            break;
+            
 
         case "zero":
-            if (num1){
-                display.textContent = "0";
-            }else{
-                display.textContent += "0";
-            }
-            break;
+            
 
         case "plus":
             //Clear the screen, save the number and the operator
@@ -109,6 +69,7 @@ buttons.addEventListener("click", (e) => {
                 num2 = parseInt(display.textContent);
                 display.textContent = "";
                 display.textContent = operate(num1, num2, operator);
+                answerDisplayed = true;
 
                 //reset numbers???
                 num1 = parseInt(display.textContent);
@@ -118,9 +79,10 @@ buttons.addEventListener("click", (e) => {
                 display.textContent = "";
             }
 
-            
-
             break;
+
+        case "minus":
+
 
         case "equals":
             num2 = parseInt(display.textContent);
